9342812143272669e8458dbe30d1bfb4e361c380
tomAradLobyd <tom.arad@lobyd.com>
2023-10-27 21:37:25 -0700

    basic functioanlity finished

---
+++ quiz-app/src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./App.css";
+import quizData from "./data/quizData";
+
+import "./App.css";
+
+function App() {
+  const [index, setIndex] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [points, setPoints] = useState(0);
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (selectedOption === null) {
+      alert("please choose an option");
+      return;
+    }
+
+    // Check if the selected option matches the correct answer for the current question
+    if (selectedOption === quizData[index].correctAnswer) {
+      // Increase points if the answer is correct
+      setPoints((curPoints) => curPoints + 1);
+    } else {
+      alert(`The correct answer was: ${quizData[index].correctAnswer}`);
+    }
+
+    // Move to the next question or end the quiz
+    setIndex((curIndex) => curIndex + 1);
+    setSelectedOption(null); // Reset the selected option for the next question
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Tom's Quiz</h1>
+      </div>
+
+      {index < quizData.length ? (
+        <div className="quiz-container">
+          <div>
+            <h2 className="question">{quizData[index].question}</h2>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="options">
+              {quizData[index].options.map((option) => (
+                <label
+                  key={option}
+                  className={`option-label ${
+                    option === selectedOption ? "selected-option" : ""
+                  }`}
+                  onClick={() => handleOptionClick(option)}
+                >
+                  <input
+                    type="radio"
+                    name="option"
+                    value={option}
+                    checked={option === selectedOption}
+                  />
+                  <span className="option-label-custom">{option}</span>
+                </label>
+              ))}
+            </div>
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </form>
+          <div className="points">
+            <h3>Points: {points}</h3>
+          </div>
+        </div>
+      ) : (
+        <div className="quiz-complete-message">
+          <h3>Quiz Complete</h3>
+          <p className="score">
+            You scored {points} out of {quizData.length} points.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ quiz-app/src/data/quizData.js
@@ -0,0 +1,29 @@
+const quizData = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      correctAnswer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Earth", "Mars", "Venus", "Jupiter"],
+      correctAnswer: "Mars",
+    },
+    {
+      question: "What is the largest mammal in the world?",
+      options: ["Elephant", "Giraffe", "Blue Whale", "Hippopotamus"],
+      correctAnswer: "Blue Whale",
+    },
+    {
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "Leo Tolstoy"],
+      correctAnswer: "William Shakespeare",
+    },
+    {
+      question: "What is the chemical symbol for gold?",
+      options: ["Au", "Ag", "Fe", "Hg"],
+      correctAnswer: "Au",
+    },
+  ];
+  
+  export default quizData;
